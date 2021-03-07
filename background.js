8c1438471d8267866ae635cfeb1a883a3ee50d8d
Elizabeth Coughlin <80206691+ecoughli@users.noreply.github.com>
2021-03-07 13:18:08 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
 const CLIENT_ID = encodeURIComponent('');
 const RESPONSE_TYPE = encodeURIComponent('id_token');
 const REDIRECT_URI = encodeURIComponent('https://cdcekdbbljkeooejblihmcbdelkihpej.chromiumapp.org/')
