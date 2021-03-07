d134fbcd9d222ee349d2cbca25d9fec2441624fb
Elizabeth Coughlin <80206691+ecoughli@users.noreply.github.com>
2021-03-07 15:11:11 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
-const app = firebase.initializeApp(config);
-const auth = app.auth();
-const signInWithPopup = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  return auth.signInWithPopup(provider).catch((error) => {
-    console.log(error);
+// Initialize Firebase
+var config = {
+  apiKey: '<AIzaSyByYtt9wPyLGORySDMcUc4-TciIvJvQ3b4>',
+  databaseURL: '<https://assignment-tracker-hackitapart.firebaseio.com>',
+  storageBucket: '<assignment-tracker-hackitapart.appspot.com>'
+};
+firebase.initializeApp(config);
+
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log('User state change detected from the Background script of the Chrome Extension:', user);
   });
+}
+
+window.onload = function() {
+  initApp();
 };
